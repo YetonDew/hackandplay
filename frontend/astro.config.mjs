2bd7c6283af4443ae43584ebe5c7586c399025c5
YetonDew <ariasjefferson@hotmail.com>
2025-10-24 16:16:07 +0200

    signIn and signup basic interfa.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
+  integrations: [react()],
   vite: { plugins: [tailwind()] },
 });
